2db78e1d320bff8dbebcfee986cae18e55d1d2d9
ianfariaa <159829854+ianfariaa@users.noreply.github.com>
2024-02-13 16:38:09 -0300

    Add files via upload
    É um Projeto feito em python, e usa condições alinhadas. Embora não tenha uma interface que mostra do triângulo em si, o projeto pode dizer se um triangulo é formado com base nas suas respostas.

---
+++ ex42.py
@@ -0,0 +1,16 @@
+m1 = float(input('Primeiro segmento:'))
+m2 = float(input('Segundo segmento:'))
+m3 = float(input('Terceiro segmento:'))
+if m1 < m2 + m3 and m2 < m1 + m3 and m3 < m1 + m2:
+    print('Os seguimentos pode formar um triangulo', end='')
+    if m1 == m2 == m3:
+        print('EQUILATERO!')
+    elif m1 != m2 != m3 != m1:
+        print(' ESCALENO!')
+    else:
+        print(' ISÓSCELES!')
+else:
+    print('Os seguimentos não pode formar um triangulo')
+
+# Isso é uma forma diferente de usar as condições alinhadas.É possível colocar um if dentro de outro if.
+# Como se fossem dois caminhos, um é sem saída (else), e o outro tem como prosseguir de várias maneiras.
